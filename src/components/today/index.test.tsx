a49b8ea5d7cbe693e41ada8ed503428e37f99210
Ali Haghighatkhah <ali@lightcurve.io>
2021-12-11 03:10:20 +0100

    Create Today component

---
+++ src/components/today/index.test.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Today from '.';
+
+interface Main {
+  [key: string]: number
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+type Info = {
+  weather: Weather[],
+  main: Main,
+};
+
+type Props = {
+  info: Info,
+};
+
+test('renders today\'s info correctly', () => {
+  const props: Props = {
+    info: {
+      weather: [{
+        id: 1,
+        main: 'Clear',
+        description: 'description',
+        icon: 'icon',
+      }],
+      main: {
+        temp_min: 250,
+        temp_max: 260,
+        temp: 255,
+      },
+    },
+  };
+  const { container } = render(<Today {...props} />);
+  const img = container.getElementsByTagName('img');
+  expect(img[0]).toHaveAttribute('alt', 'statusClear');
+
+  const h5 = container.getElementsByTagName('h5');
+  expect(h5[0]).toHaveTextContent('Clear');
+  expect(h5[1]).toHaveTextContent('-23° / -13°');
+
+  const h1 = container.getElementsByTagName('h1');
+  expect(h1[0]).toHaveTextContent('-18°');
+});
---
+++ src/components/today/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Illustration from '../toolbox/illustration';
+import Temperature from '../toolbox/temperature';
+import './today.css';
+
+interface Main {
+  [key: string]: number
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+type Info = {
+  weather: Weather[],
+  main: Main,
+};
+
+type Props = {
+  info: Info,
+};
+
+const Today = ({ info }: Props) => (
+  <section className="today" data-testid="today">
+    <figure data-testid="status">
+      <Illustration
+        name={info ? `status${info.weather[0].main}` : 'loading'}
+      />
+    </figure>
+    <main>
+      <div>
+        <h5>{ info ? info.weather[0].main : '-' }</h5>
+        <h5>
+          <Temperature 
+            value={info && [info.main.temp_min, info.main.temp_max]}
+          />
+        </h5>
+      </div>
+      <div>
+        <h1>
+          <Temperature
+            value={info && info.main.temp}
+          />
+        </h1>
+      </div>
+    </main>
+  </section>
+);
+
+export default Today;
