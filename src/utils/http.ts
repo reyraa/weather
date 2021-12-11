96b13caede02b673e8e68ac53c26c71e31536a39
Ali Haghighatkhah <ali@lightcurve.io>
2021-12-11 03:07:58 +0100

    Add HTTP utility

---
+++ src/utils/http.ts
@@ -0,0 +1,29 @@
+type props = {
+  baseUrl?: string,
+  path: string,
+  // params: string | string[][] | Record<string, string> | URLSearchParams | undefined,
+  method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE' | 'OPTIONS' | 'HEAD' | undefined,
+};
+
+/**
+ * Makes HTTP api call
+ *
+ * @param {string} baseUrl - optional service base url
+ * @param {string} path - api endpoint
+ * @param {string} method - HTTP method
+ * @param {string} params - HTTP call parameters
+ * @returns {Promise} - if success it returns data,
+ * if fails on server it throws an error,
+ *
+ */
+const http = ({
+  baseUrl = '', path, method = 'GET',
+}: props) =>
+  fetch(`${baseUrl}${path}`, {
+    method,
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+export default http;
